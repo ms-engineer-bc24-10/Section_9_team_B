466cf61f2672d42870945b14e8cb2e395b20fb92
ririri-s <shijukara.chun@gmail.com>
2025-01-01 23:48:36 +0900

    test: #106 サブスク用テストを追加

--- backend/config/test_settings.py
+++ backend/config/test_settings.py
@@ -16,6 +16,8 @@ DATABASES = {
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")
 
+STRIPE_API_BASE = os.getenv("STRIPE_API_BASE", "http://localhost:12111")
+
 
 LOGGING = {
     "version": 1,
